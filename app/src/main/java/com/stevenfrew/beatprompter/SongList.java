d67b6598898063ec5519e1633ad6d570cb900860
steve <steven.fullhouse@gmail.com>
2018-07-01 12:52:17 +0100

    more mess

--- app/src/main/java/com/stevenfrew/beatprompter/SongList.java
+++ app/src/main/java/com/stevenfrew/beatprompter/SongList.java
@@ -90,8 +90,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.File;
