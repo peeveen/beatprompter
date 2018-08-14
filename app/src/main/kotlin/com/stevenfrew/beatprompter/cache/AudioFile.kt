ac5ecb2555e211e17a7fb752dd074cbea5fc4631
Steven Frew <steven.fullhouse@gmail.com>
2018-08-14 10:21:44 +0100

    more reorg

--- app/src/main/kotlin/com/stevenfrew/beatprompter/cache/AudioFile.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/cache/AudioFile.kt
@@ -6,12 +6,13 @@ import com.stevenfrew.beatprompter.R
 import com.stevenfrew.beatprompter.cloud.SuccessfulCloudDownloadResult
 import org.w3c.dom.Document
 import org.w3c.dom.Element
+import java.io.File
 
 class AudioFile : CachedCloudFile {
 
     @Throws(InvalidBeatPrompterFileException::class)
     internal constructor(result: SuccessfulCloudDownloadResult) : super(result) {
-        verifyAudioFile()
+        verifyAudioFile(mFile,mName)
     }
 
     internal constructor(e: Element) : super(e)
@@ -22,20 +23,19 @@ class AudioFile : CachedCloudFile {
         element.appendChild(audioFileElement)
     }
 
-    @Throws(InvalidBeatPrompterFileException::class)
-    private fun verifyAudioFile() {
-        try {
-            // Try to read the length of the track. If it fails, it's not an audio file.
-            val mmr = MediaMetadataRetriever()
-            mmr.setDataSource(mFile.absolutePath)
-            mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
-        } catch (e: Exception) {
-            throw InvalidBeatPrompterFileException(BeatPrompterApplication.getResourceString(R.string.notAnAudioFile, mName))
-        }
-
-    }
-
     companion object {
         const val AUDIOFILE_ELEMENT_TAG_NAME = "audiofile"
+
+        @Throws(InvalidBeatPrompterFileException::class)
+        private fun verifyAudioFile(file: File,name:String) {
+            try {
+                // Try to read the length of the track. If it fails, it's not an audio file.
+                val mmr = MediaMetadataRetriever()
+                mmr.setDataSource(file.absolutePath)
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+            } catch (e: Exception) {
+                throw InvalidBeatPrompterFileException(BeatPrompterApplication.getResourceString(R.string.notAnAudioFile, name))
+            }
+        }
     }
 }
