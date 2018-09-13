90dd2ba93b780c87def0990354387d1866e02166
steve <steven.fullhouse@gmail.com>
2018-09-13 13:53:03 +0100

    Fixed band leader pref ... no long initialises list of paired devices just once.

--- app/src/main/kotlin/com/stevenfrew/beatprompter/ui/pref/BandLeaderPreference.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/ui/pref/BandLeaderPreference.kt
@@ -9,11 +9,10 @@ import android.widget.ArrayAdapter
 import com.stevenfrew.beatprompter.comm.bluetooth.BluetoothManager
 
 class BandLeaderPreference(context: Context, attrs: AttributeSet) : ListPreference(context, attrs) {
-    private val mPairedDevices:List<BluetoothDevice> = BluetoothManager.getPairedDevices()
-
     override fun onPrepareDialogBuilder(builder: AlertDialog.Builder) {
-        entries = mPairedDevices.map{it.name}.toTypedArray()
-        entryValues = mPairedDevices.map{it.address}.toTypedArray()
+        val pairedDevices=BluetoothManager.getPairedDevices()
+        entries = pairedDevices.map{it.name}.toTypedArray()
+        entryValues = pairedDevices.map{it.address}.toTypedArray()
         val listAdapter = ArrayAdapter(context,android.R.layout.select_dialog_singlechoice, entries)
         builder.setAdapter(listAdapter, this)
         super.onPrepareDialogBuilder(builder)
