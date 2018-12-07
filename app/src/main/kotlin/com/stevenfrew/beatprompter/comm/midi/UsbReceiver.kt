3aa95f0b42f4545bdd142a5833c5b7491b93a95f
Steven Frew <steven.fullhouse@gmail.com>
2018-12-07 12:36:56 +0000

    Getting closer to solving the USB MIDI mystery.

--- app/src/main/kotlin/com/stevenfrew/beatprompter/comm/midi/UsbReceiver.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/comm/midi/UsbReceiver.kt
@@ -14,7 +14,7 @@ class UsbReceiver(private val mConnection: UsbDeviceConnection, private val mEnd
         var dataRead: Int
         val wasteBuffer = ByteArray(mEndpoint.maxPacketSize)
         do {
-            dataRead = mConnection.bulkTransfer(mEndpoint, wasteBuffer, wasteBuffer.size, 250)
+            dataRead = mConnection.bulkTransfer(mEndpoint, wasteBuffer, wasteBuffer.size, 1000)
             if (dataRead > 0)
                 bufferClear += dataRead
         } while (dataRead > 0 && bufferClear < 1024)
@@ -25,10 +25,14 @@ class UsbReceiver(private val mConnection: UsbDeviceConnection, private val mEnd
     }
 
     override fun receiveMessageData(buffer: ByteArray, offset: Int, maximumAmount: Int): Int {
-        val newArray = if (offset == 0) buffer else ByteArray(maximumAmount)
-        return mConnection.bulkTransfer(mEndpoint, newArray, maximumAmount, 1000).also {
-            if (offset != 0)
-                System.arraycopy(newArray, 0, buffer, offset, maximumAmount)
+        val maxRead = maximumAmount - offset
+        val newArray = if (offset == 0) buffer else ByteArray(maxRead)
+        return mConnection.bulkTransfer(mEndpoint,
+                newArray,
+                maxRead,
+                250).also {
+            if (it > 0 && offset != 0)
+                System.arraycopy(newArray, 0, buffer, offset, it)
         }
     }
 
