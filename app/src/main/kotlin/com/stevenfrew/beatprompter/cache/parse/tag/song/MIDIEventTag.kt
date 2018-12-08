3fa6d56f21037ed77431f5e3ac21d7bc859fbf75
steve <steven.fullhouse@gmail.com>
2018-12-08 14:15:06 +0000

    Fixed MIDI beat offset.

--- app/src/main/kotlin/com/stevenfrew/beatprompter/cache/parse/tag/song/MIDIEventTag.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/cache/parse/tag/song/MIDIEventTag.kt
@@ -128,7 +128,7 @@ class MIDIEventTag internal constructor(name: String,
                         // Might be in the beat format
                         val offsetChars = trimmedOffsetString.toCharArray()
                         val upDiff = offsetChars.count { it == '>' }
-                        val downDiff = offsetChars.count { it == '<' }
+                        val downDiff = -offsetChars.count { it == '<' }
                         val badCharsFound = offsetChars.any { it != '<' && it != '>' }
                         if (badCharsFound)
                             throw MalformedTagException(R.string.non_beat_characters_in_midi_offset)
