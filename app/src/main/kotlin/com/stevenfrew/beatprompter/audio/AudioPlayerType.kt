eefcbb876cde02e5ec8c2badb088cdbc58997330
Steven Frew <steven.fullhouse@gmail.com>
2023-08-12 16:39:18 +0100

    audioplayers

---
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/audio/AudioPlayerType.kt
@@ -0,0 +1,4 @@
+package com.stevenfrew.beatprompter.audio
+enum class AudioPlayerType {
+	MediaPlayer, ExoPlayer
+}
---
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/audio/MediaPlayerAudioPlayer.kt
@@ -0,0 +1,62 @@
+package com.stevenfrew.beatprompter.audio
+
+import android.content.Context
+import android.media.MediaPlayer
+import com.stevenfrew.beatprompter.R
+import java.io.File
+import java.io.FileInputStream
+
+class MediaPlayerAudioPlayer:AudioPlayer {
+	private val mInternalPlayer:MediaPlayer
+	constructor(context:Context){
+		// Silence player
+		mInternalPlayer=MediaPlayer.create(context, R.raw.silence).apply{
+			setVolume(0.01f,0.01f)
+			isLooping = true
+			prepare()
+		}
+	}
+
+	constructor(file: File, volume: Int){
+		// File player
+		mInternalPlayer=MediaPlayer().apply {
+			FileInputStream(file.absolutePath)
+			.use { stream ->
+				setDataSource(stream.fd)
+				prepare()
+				seekTo(0)
+				setVolume(0.01f * volume, 0.01f * volume)
+				isLooping = false
+			}
+		}
+	}
+
+	override fun seekTo(ms: Long) {
+		mInternalPlayer.seekTo(ms.toInt())
+	}
+
+	override fun stop() {
+		mInternalPlayer.stop()
+	}
+
+	override fun start() {
+		mInternalPlayer.start()
+	}
+
+	override fun pause() {
+		mInternalPlayer.pause()
+	}
+
+	override val isPlaying: Boolean
+		get() = mInternalPlayer.isPlaying
+	override val duration: Long
+		get() = mInternalPlayer.duration.toLong()
+
+	override fun release() {
+		mInternalPlayer.release()
+	}
+
+	override fun setVolume(volume: Int) {
+		mInternalPlayer.setVolume(volume*0.01f,volume*0.01f)
+	}
+}
