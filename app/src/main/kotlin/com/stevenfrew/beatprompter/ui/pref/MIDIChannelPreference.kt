aa4b2bb88e38687697e85fd553da56c97df58137
Steven Frew <steven.fullhouse@gmail.com>
2023-08-13 15:54:58 +0100

    midi prefs

--- app/src/main/kotlin/com/stevenfrew/beatprompter/ui/pref/MIDIChannelPreference.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/ui/pref/MIDIChannelPreference.kt
@@ -1,6 +1,7 @@
 package com.stevenfrew.beatprompter.ui.pref
 
 import android.content.Context
+import android.content.res.TypedArray
 import androidx.preference.DialogPreference
 import android.util.AttributeSet
 import android.view.View
@@ -9,116 +10,42 @@ import android.widget.GridLayout
 import android.widget.ToggleButton
 import com.stevenfrew.beatprompter.R
 
-class MIDIChannelPreference : DialogPreference, CompoundButton.OnCheckedChangeListener {
-    private var mCurrentValue: Int = 0
-    private val mSingleSelect: Boolean
-    private var mView: View? = null
+class MIDIChannelPreference : DialogPreference {
+	private var mDefaultValue:Int=0
+  val singleSelect: Boolean
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        dialogLayoutResource = R.layout.midi_channel_preference_dialog
-        setPositiveButtonText(android.R.string.ok)
-        setNegativeButtonText(android.R.string.cancel)
-        mSingleSelect = getSingleSelectSetting(attrs)
-        mCurrentValue = if (mSingleSelect) 1 else 65535
-    }
+	constructor(context: Context?):	this(context, null)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        dialogLayoutResource = R.layout.midi_channel_preference_dialog
-        setPositiveButtonText(android.R.string.ok)
-        setNegativeButtonText(android.R.string.cancel)
-        mSingleSelect = getSingleSelectSetting(attrs)
-        mCurrentValue = if (mSingleSelect) 1 else 65535
-    }
+	constructor(context: Context?, attrs: AttributeSet?):this(context, attrs, 0)
 
+	constructor(	context: Context?, attrs: AttributeSet?,	defStyleAttr: Int	):this(context, attrs, defStyleAttr, defStyleAttr)
 
-    private fun getSingleSelectSetting(attrs: AttributeSet): Boolean {
-        val resourceID = attrs.getAttributeResourceValue(sfns, "singleSelect", 0)
-        return if (resourceID == 0)
-            attrs.getAttributeBooleanValue(sfns, "singleSelect", false)
-        else
-            context.getString(resourceID).toBoolean()
-    }
-/*
-    override fun onBindDialogViewHolder(view: PreferenceView) {
-        super.onBindDialogView(view)
-        mView = view
-        view.findViewById<GridLayout>(R.id.midiGrid).apply {
-            useDefaultMargins = false
-            alignmentMode = GridLayout.ALIGN_BOUNDS
-            isRowOrderPreserved = false
-        }
-        mCurrentValue = this.getPersistedInt(if (mSingleSelect) 1 else 65535)
-        repeat(16) {
-            val tb = view.findViewById<ToggleButton>(toggleIDs[it])
-            val set = mCurrentValue and (1 shl it) != 0
-            tb.isChecked = set
-            if (mSingleSelect && set)
-                tb.isEnabled = false
-            tb.setOnCheckedChangeListener(this)
-        }
-    }
-*/
-    override fun onSetInitialValue(restorePersistedValue: Boolean, defaultValue: Any?) {
-        if (restorePersistedValue) {
-            // Restore existing state
-            mCurrentValue = this.getPersistedInt(if (mSingleSelect) 1 else 65535)
-        } else {
-            // Set default state from the XML attribute
-            mCurrentValue = defaultValue as Int
-            persistInt(mCurrentValue)
-        }
-    }
-/*
-    override fun onDialogClosed(positiveResult: Boolean) {
-        // When the user selects "OK", persist the new value
-        if (positiveResult) {
-            persistInt(mCurrentValue)
-        }
-    }
-*/
-    override fun onCheckedChanged(buttonView: CompoundButton, isNowChecked: Boolean) {
-        if (mSingleSelect && isNowChecked) {
-            repeat(16) {
-                mView!!.findViewById<ToggleButton>(toggleIDs[it]).apply {
-                    if (this !== buttonView) {
-                        if (isChecked && !isEnabled) {
-                            isChecked = false
-                            isEnabled = true
-                        }
-                    }
-                }
-            }
-            buttonView.isEnabled = false
-        }
-        repeat(16) {
-            if (toggleIDs[it] == buttonView.id) {
-                mCurrentValue = if (isNowChecked)
-                    mCurrentValue or (1 shl it)
-                else
-                    mCurrentValue and (1 shl it).inv()
-                return
-            }
-        }
+	constructor(
+		context: Context?, attrs: AttributeSet?,
+		defStyleAttr: Int, defStyleRes: Int
+	) :	super(context!!, attrs, defStyleAttr, defStyleRes){
+		singleSelect = attrs?.getAttributeBooleanValue(sfns, "singleSelect", false) ?: false
+	}
+
+	fun setPreferenceValue(fontSize:Int) {
+		persistInt(fontSize)
+	}
+
+	fun getPreferenceValue():Int{
+		return getPersistedInt(mDefaultValue)
+	}
+
+	override fun onGetDefaultValue(a: TypedArray, index: Int): Any {
+		mDefaultValue=a.getString(index)!!.toInt()
+		return mDefaultValue
+	}
+
+    override fun onSetInitialValue(defaultValue: Any?) {
+				if(defaultValue is Int)
+					setPreferenceValue(defaultValue)
     }
 
     companion object {
-        private const val sfns = "http://com.stevenfrew/"
-        private val toggleIDs = intArrayOf(
-                R.id.midiChannel1Button,
-                R.id.midiChannel2Button,
-                R.id.midiChannel3Button,
-                R.id.midiChannel4Button,
-                R.id.midiChannel5Button,
-                R.id.midiChannel6Button,
-                R.id.midiChannel7Button,
-                R.id.midiChannel8Button,
-                R.id.midiChannel9Button,
-                R.id.midiChannel10Button,
-                R.id.midiChannel11Button,
-                R.id.midiChannel12Button,
-                R.id.midiChannel13Button,
-                R.id.midiChannel14Button,
-                R.id.midiChannel15Button,
-                R.id.midiChannel16Button)
-    }
+			private const val sfns = "http://com.stevenfrew/"
+		}
 }
--- app/src/main/kotlin/com/stevenfrew/beatprompter/ui/pref/MidiSettingsFragment.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/ui/pref/MidiSettingsFragment.kt
@@ -1,5 +1,17 @@
 package com.stevenfrew.beatprompter.ui.pref
 
+import androidx.preference.Preference
 import com.stevenfrew.beatprompter.R
 
-class MidiSettingsFragment : BaseSettingsFragment(R.xml.midipreferences)
+class MidiSettingsFragment : BaseSettingsFragment(R.xml.midipreferences) {
+
+	override fun onDisplayPreferenceDialog(preference: Preference) {
+		if (preference is MIDIChannelPreference) {
+			val f: MIDIChannelPreferenceDialog = MIDIChannelPreferenceDialog.newInstance(preference.key, preference.singleSelect)
+			f.setTargetFragment(this, 0)
+			f.show(parentFragmentManager, "seekBarDialog")
+		} else {
+			super.onDisplayPreferenceDialog(preference)
+		}
+	}
+}
