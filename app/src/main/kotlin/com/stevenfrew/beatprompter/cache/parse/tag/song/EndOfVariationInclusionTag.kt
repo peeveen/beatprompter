9488790c08eb1c12f52ccc812a663b8a0e708f5d
Steven Frew <steven.fullhouse@gmail.com>
2024-09-21 12:28:00 +0100

    added variation inclusion tags

---
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/cache/parse/tag/song/EndOfVariationInclusionTag.kt
@@ -0,0 +1,21 @@
+package com.stevenfrew.beatprompter.cache.parse.tag.song
+
+import com.stevenfrew.beatprompter.cache.parse.tag.OncePerLine
+import com.stevenfrew.beatprompter.cache.parse.tag.StartedBy
+import com.stevenfrew.beatprompter.cache.parse.tag.Tag
+import com.stevenfrew.beatprompter.cache.parse.tag.TagName
+import com.stevenfrew.beatprompter.cache.parse.tag.TagType
+import com.stevenfrew.beatprompter.cache.parse.tag.find.Type
+
+@StartedBy(StartOfVariationInclusionTag::class)
+@TagName("varend", "varstop", "end_of_variation")
+@TagType(Type.Directive)
+@OncePerLine
+/**
+ * Tag that defines the end of a variation inclusion section.
+ */
+class EndOfVariationInclusionTag internal constructor(
+	name: String,
+	lineNumber: Int,
+	position: Int
+) : Tag(name, lineNumber, position)
---
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/cache/parse/tag/song/StartOfVariationInclusionTag.kt
@@ -0,0 +1,29 @@
+package com.stevenfrew.beatprompter.cache.parse.tag.song
+
+import com.stevenfrew.beatprompter.cache.parse.tag.EndedBy
+import com.stevenfrew.beatprompter.cache.parse.tag.OncePerLine
+import com.stevenfrew.beatprompter.cache.parse.tag.Tag
+import com.stevenfrew.beatprompter.cache.parse.tag.TagName
+import com.stevenfrew.beatprompter.cache.parse.tag.TagType
+import com.stevenfrew.beatprompter.cache.parse.tag.find.Type
+import com.stevenfrew.beatprompter.util.splitAndTrim
+
+@EndedBy(EndOfVariationExclusionTag::class)
+@TagName("varstart", "start_of_variation")
+@TagType(Type.Directive)
+@OncePerLine
+/**
+ * Tag that defines the start of a variation inclusion section
+ */
+class StartOfVariationInclusionTag internal constructor(
+	name: String,
+	lineNumber: Int,
+	position: Int,
+	value: String
+) : Tag(
+	name,
+	lineNumber,
+	position,
+) {
+	val variations: List<String> = value.splitAndTrim(",").filter { it.isNotBlank() }
+}
