67065d7790c69a4a0697fd1b13709c694b8602b0
steven.frew <steven.frew@corporatemodelling.com>
2024-12-16 11:49:14 +0000

    no longer replaces accidentals in unrecognized chords

--- app/src/main/kotlin/com/stevenfrew/beatprompter/chord/UnknownChord.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/chord/UnknownChord.kt
@@ -5,7 +5,7 @@ class UnknownChord(private val chord: String) : IChord {
 		alwaysUseSharps: Boolean,
 		useUnicodeAccidentals: Boolean,
 		majorOrMinorRootOnly: Boolean
-	): String = ChordUtils.replaceAccidentals(chord, useUnicodeAccidentals)
+	): String = ChordUtils.replaceAccidentals(chord, false)
 
 	override fun transpose(transpositionMap: Map<Note, Note>): IChord = this
 }
