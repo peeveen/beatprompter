d5138b98dfa4d933324192d7ca362db1d7ec3e53
steven.frew <steven.frew@corporatemodelling.com>
2025-03-10 15:35:00 +0000

    code tidy

--- app/src/main/kotlin/com/stevenfrew/beatprompter/ui/filter/TagFilter.kt
+++ app/src/main/kotlin/com/stevenfrew/beatprompter/ui/filter/TagFilter.kt
@@ -2,7 +2,7 @@ package com.stevenfrew.beatprompter.ui.filter
 
 import com.stevenfrew.beatprompter.cache.SongFile
 
-class TagFilter(tag: String, songs: MutableList<SongFile>) :
+class TagFilter(tag: String, songs: List<SongFile>) :
 	SongFilter(tag, songs.map { it to null }, true) {
 	override fun equals(other: Any?): Boolean = other is TagFilter && name == other.name
 	override fun hashCode(): Int = javaClass.hashCode()
